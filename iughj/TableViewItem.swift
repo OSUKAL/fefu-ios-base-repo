6601756c69cdea9bde494ae8312549ef4cf7f1dd
OSUKAL <81548359+OSUKAL@users.noreply.github.com>
2021-11-03 19:31:49 +1000

    hw-1 + hw-2 recommit

---
+++ iughj/TableViewItem.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewItem.swift
+//  iughj
+//
+//  Created by wsr2 on 30.10.2021.
+//
+
+import UIKit
+
+class TableViewItem: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
