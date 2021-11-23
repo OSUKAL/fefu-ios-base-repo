cfb80757bb7774367673f2bad4a2c80038d6fae4
OSUKAL <81548359+OSUKAL@users.noreply.github.com>
2021-11-23 18:25:29 +1000

    hw3.1

--- iughj/ViewController.swift
+++ iughj/ViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import CoreLocation
+import MapKit
 
 class ViewController: UIViewController {
 
@@ -69,3 +71,104 @@ extension ViewController: UITableViewDataSource, UITableViewDelegate {
         }
     }
 }
+
+
+class MapNavigation: UIViewController {
+    
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let locationManager: CLLocationManager = {
+        let manager = CLLocationManager()
+        
+        manager.desiredAccuracy = kCLLocationAccuracyBestForNavigation
+        
+        return manager
+    }()
+    
+    var userLocation: CLLocation? {
+        didSet{
+            guard let userLocation = userLocation else {
+                return
+            }
+            
+            let region = MKCoordinateRegion(center: userLocation.coordinate,
+                                            latitudinalMeters: 500,
+                                            longitudinalMeters: 500)
+            
+            mapView.setRegion(region, animated: true)
+            
+            userLocationsHistory.append(userLocation)
+        }
+    }
+    
+    fileprivate var userLocationsHistory: [CLLocation] = [] {
+        didSet {
+            let coordinates = userLocationsHistory.map { $0.coordinate }
+            
+            let route = MKPolyline(coordinates: coordinates, count: coordinates.count)
+            
+            route.title = "Ваш маршрут"
+            
+            mapView.addOverlay(route)
+        }
+    }
+    
+    private let userLocationIdentifier = "user_icon"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        locationManager.delegate = self
+        locationManager.requestAlwaysAuthorization()
+        locationManager.startUpdatingLocation()
+        
+        mapView.showsUserLocation = true
+        mapView.delegate = self
+    }
+}
+
+extension MapNavigation: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let currentLocation = locations.first else {
+            return
+        }
+        
+        print("received device location:", currentLocation.coordinate)
+        userLocation = currentLocation
+    }
+}
+
+extension MapNavigation: MKMapViewDelegate {
+    
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        if let polyline = overlay as? MKPolyline {
+            let render = MKPolylineRenderer(polyline: polyline)
+            render.fillColor = UIColor.blue
+            render.strokeColor = UIColor.blue
+            render.lineWidth = 4
+            
+            return render
+        }
+        
+        return MKOverlayRenderer(overlay: overlay)
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if let annotation = annotation as? MKUserLocation {
+            let dequedView = mapView.dequeueReusableAnnotationView(withIdentifier: userLocationIdentifier)
+            
+            let view = dequedView ?? MKAnnotationView(annotation: annotation, reuseIdentifier: userLocationIdentifier)
+            
+            view.image = UIImage(named: "ic_user_location")
+            
+            return view
+        }
+        
+        return nil
+    }
+}
+
+
+
