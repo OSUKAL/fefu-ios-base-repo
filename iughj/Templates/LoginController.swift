fd08970ae3a9be96f3847668f8f21dfe7ddfd086
OSUKAL <81548359+OSUKAL@users.noreply.github.com>
2021-12-23 17:16:51 +1000

    Add files via upload

---
+++ iughj/Templates/LoginController.swift
@@ -0,0 +1,68 @@
+import UIKit
+
+class LoginController: UIViewController {
+
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passTextField: UITextField!
+    
+    struct RegUserModel: Decodable, Encodable {
+        let login: String
+        let pasword: String
+        let name: String
+        let genre: Int
+    }
+    
+    struct RegUserResp: Decodable {
+        let token: String
+    }
+
+    @IBAction func didLoginBtnTap(_ sender: Any) {
+
+        let username = loginTextField.text ?? ""
+        let passw = passTextField.text ?? ""
+
+        if (username == "" || passw == "") {
+            let alert = UIAlertController(title: "Wrong data", message: "Fields cannot be empty", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true)
+            return
+        }
+        
+        let userData =  UserLoginReq(login: username, password: passw)
+        do {
+            let jsonUserData = try AuthRegUrlManager.encoder.encode(userData)
+            
+            AuthRegUrlManager.instance.login(jsonUserData) { user in
+                print(user.token)
+                AuthRegDataManager.intance.saveUser(login: username, password: passw, pabKey: user.token)
+                DispatchQueue.main.async {
+                    let view = TabsViewController(nibName: "TabsViewController", bundle: nil)
+                    view.modalPresentationStyle = .fullScreen
+                    self.present(view, animated: true, completion: nil)
+                }
+            }
+        } catch {
+            print("error: unpacking json")
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        selfInit()
+    }
+    
+    private func fetchUserData() {
+        loginTextField.text = AuthRegDataManager.intance.getKey(nameOfKey: AuthRegDataManager.userNameKey) ?? ""
+        passTextField.text = AuthRegDataManager.intance.getKey(nameOfKey: AuthRegDataManager.userPassKey) ?? ""
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    private func selfInit() {
+        let backBtn = UIBarButtonItem()
+        backBtn.title = ""
+        navigationController?.navigationBar.topItem?.backBarButtonItem = backBtn
+    }
+}
